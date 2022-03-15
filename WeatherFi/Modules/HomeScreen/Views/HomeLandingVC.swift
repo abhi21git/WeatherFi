f8ed2cd832c9d5df1f1a5f5c97930b1e527df70d
Abhishek Maurya <42537316+abhi21git@users.noreply.github.com>
2022-03-15 23:59:44 +0530

    Project initial structuring

---
+++ WeatherFi/Modules/HomeScreen/Views/HomeLandingVC.swift
@@ -0,0 +1,46 @@
+//
+//  HomeLandingVC.swift
+//  WeatherFi
+//
+//  Created by Abhishek Maurya on 15/03/22.
+//
+
+import UIKit
+
+//MARK: - HomeLandingVC
+class HomeLandingVC: UIViewController {
+    //MARK: - Properties
+
+
+    //MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Add code here
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // Add code here
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // Add code here
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        // Add code here
+
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        // Add code here
+    }
+    
+    //MARK: - Methods
+
+
+}
+
+//MARK: - HomeLandingVC Extension
+extension HomeLandingVC {
+    
+}
